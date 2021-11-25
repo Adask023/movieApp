51529f34aa46ba1c9f6b3dba505070d66add07ca
Adam Kudłacik <adam.kud@wp.pl>
2021-11-25 03:49:50 +0100

    add responsive menu logic

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -3,11 +3,37 @@ import { ToogleThemeButton } from "../ToogleThemeButton";
 import "../../styles/Nav.scss";
 import { NavItem } from "./NavItem/NavItem";
 import { ROUTES } from "../../constants/routes";
+import { useEffect, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
 
 interface Props {}
 
 export const Nav = (props: Props) => {
   const { dark } = useThemeContext();
+  const [isMobile, setIsMobile] = useState(false);
+  const [isMobileNav, setIsMobileNav] = useState(false);
+
+  useEffect(() => {
+    function handleResize() {
+      // console.log("resized to: ", window.innerWidth, "x", window.innerHeight);
+
+      if (window.innerWidth < 900) {
+        setIsMobile(true);
+      } else {
+        setIsMobile(false);
+        setIsMobileNav(false);
+      }
+    }
+
+    handleResize();
+    window.addEventListener("resize", handleResize);
+  }, []);
+
+  const toggleMobileNav = () => {
+    setIsMobileNav(!isMobileNav);
+  };
+
   return (
     <div>
       <div className="top__socials">
@@ -21,23 +47,68 @@ export const Nav = (props: Props) => {
         className={`${dark ? "dark__theme" : "light__theme"} nav__wrapper`}
       >
         <nav>
-          <ul className="nav__list">
-            <li>
-              <NavItem path={ROUTES.START} text="Home" />
-            </li>
-            <li>
-              <NavItem path={ROUTES.FILMS} text="Films" />
-            </li>
-            <li>
-              <NavItem path={ROUTES.FAVOURITE} text="Favourite" />
-            </li>
-            <li>
-              <NavItem path={ROUTES.SETTINGS} text="Settings" />
-            </li>
-          </ul>
+          {!isMobile ? (
+            <ul className="nav__list">
+              <li>
+                <NavItem path={ROUTES.START} text="Home" />
+              </li>
+              <li>
+                <NavItem path={ROUTES.FILMS} text="Films" />
+              </li>
+              <li>
+                <NavItem path={ROUTES.FAVOURITE} text="Favourite" />
+              </li>
+              <li>
+                <NavItem path={ROUTES.SETTINGS} text="Settings" />
+              </li>
+            </ul>
+          ) : (
+            <div className="toggle_mobile__button" onClick={toggleMobileNav}>
+              <FontAwesomeIcon icon={faBars} />
+            </div>
+          )}
         </nav>
         <ToogleThemeButton />
       </header>
+      {isMobileNav ? (
+        <div className="mobile_menu__wrapper">
+          <div className="mobile_menu">
+            <div className="toggle_mobile__button" onClick={toggleMobileNav}>
+              <FontAwesomeIcon icon={faTimes} />
+            </div>
+            <ul className="mobile_nav__list">
+              <li>
+                <NavItem
+                  handleClick={toggleMobileNav}
+                  path={ROUTES.START}
+                  text="Home"
+                />
+              </li>
+              <li>
+                <NavItem
+                  handleClick={toggleMobileNav}
+                  path={ROUTES.FILMS}
+                  text="Films"
+                />
+              </li>
+              <li>
+                <NavItem
+                  handleClick={toggleMobileNav}
+                  path={ROUTES.FAVOURITE}
+                  text="Favourite"
+                />
+              </li>
+              <li>
+                <NavItem
+                  handleClick={toggleMobileNav}
+                  path={ROUTES.SETTINGS}
+                  text="Settings"
+                />
+              </li>
+            </ul>
+          </div>
+        </div>
+      ) : null}
     </div>
   );
 };
--- src/components/Nav/NavItem/NavItem.tsx
+++ src/components/Nav/NavItem/NavItem.tsx
@@ -4,13 +4,15 @@ import { NavLink } from "react-router-dom";
 interface Props {
   path: string;
   text: string;
+  handleClick?: () => void;
 }
 
-export const NavItem = ({ path, text }: Props) => {
+export const NavItem = ({ path, text, handleClick }: Props) => {
   return (
     <>
       <NavLink
         exact
+        onClick={handleClick ? handleClick : undefined}
         className={(isActive) => "nav-Link" + (!isActive ? " unselected" : "")}
         to={path}
       >
